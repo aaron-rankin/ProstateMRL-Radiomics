297f550fdd0463cb8c00cbabe817e73e27ba27e1
Aaron Rankin <aaron.rankin@student.manchester.ac.uk>
2022-02-24 10:57:25 +0000

    Added body mask and changed formatting of histograms

--- Extraction/HistogramPlotter.py
+++ Extraction/HistogramPlotter.py
@@ -5,52 +5,61 @@ import numpy as np
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
 import os
-import radiomics
 import sys
 
 # check version of libraries (Py 3.6.5, PyRad 3.0)
 print("Python version: " + sys_version)
-print ("PyRadiomics version: " + radiomics.__version__)
 
+# patient nifti directories
 url_20f = 'D:/data/prostateMR_radiomics/nifti/new_20fractions/'
 url_20f_new = 'D:/data/prostateMR_radiomics/nifti_new/new_20fractions/'
 url_SABR = 'D:/data/prostateMR_radiomics/nifti/SABR/'
 url_SABR_new = 'D:/data/prostateMR_radiomics/nifti_new/new_SABR/'
-ptDir = os.listdir(url_20f_new)
 
+# set working directory
+url = url_20f
+
+ptDir = os.listdir(url)
+print(ptDir)
 
 # Loop through ptDir
 for i in ptDir:
-    scanWeeks = os.listdir(url_20f_new+str(i))
+    scanWeeks = os.listdir(url+str(i))
     
     # Loop through patient visits
     for j in scanWeeks:
-        niiFiles = os.listdir(url_20f_new+str(i)+"\\"+str(j))
-        # print(niiFiles)
+        niiFiles = os.listdir(url+str(i)+"\\"+str(j))
         print ("Processing: "+i+"  Timepoint: "+j)	
         imageName = i +" "+ j
-        image = url_20f_new+str(i)+"\\"+str(j)+"\\"+str(i)+"_"+str(j)+"_image.nii"
+        image = url+str(i)+"\\"+str(j)+"\\"+str(i)+"_"+str(j)+"_image.nii"
         #print ("Image: "+ imageName)
 
         # Loop through patient files
         for k in niiFiles:
-            #if "ostate" in k:
-            if "RP" in k: 
+            # load in body masks
+            if "body_mask" in k:
+                bodyMask = url + str(i) + "\\" + str(j) + "\\" + str(k)
+                readBodyMask = sitk.ReadImage(bodyMask)
+                bodyMaskArray = sitk.GetArrayFromImage(readBodyMask)
+            
+            #if "ostate" in k:          # for original patients (multiple contours)
+            if "RP" in k:               # for new patients  (one contour)
                 maskName = str(k)
                 segmentation = True
-                mask = url_20f_new+str(i)+"\\"+str(j)+"\\"+str(k)
+                mask = url+str(i)+"\\"+str(j)+"\\"+str(k)
                 print("Mask: " + maskName)
                 
                 # read in whole image
                 readImage = sitk.ReadImage(image)
                 imageArray = sitk.GetArrayFromImage(readImage)
-                #imageArray = imageArray.flatten()
+                # remove stray pixel values
+                imageArray = imageArray * bodyMaskArray
 
                 # read in mask
                 readMask = sitk.ReadImage(mask)
                 maskArray = sitk.GetArrayFromImage(readMask)
-                #maskArray = maskArray.flatten()
 
+                # for plotting intensity only in contour
                 maskedImage = maskArray * imageArray
 
                 maskedImage = maskedImage.flatten()
@@ -58,10 +67,12 @@ for i in ptDir:
                 
                 # Open figure
                 plt.figure("Intensity Histogram")
-                plt.title("MR Intensity MRL Prostate")
+                plt.title("Patient: " + i + " " + j)
                 plt.hist(imageArray, bins = 128, range=(1, imageArray.max()), facecolor = "red", alpha = 0.75, histtype = "step", density = True)
                 plt.hist(maskedImage, bins = 128, range=(1, imageArray.max()), facecolor = "blue", alpha = 0.75, histtype = "step", fill = True, density = True, label = "Prostate Contour")
                 plt.xlabel("MR Intensity")
+                plt.xlim(0,450)
+                # plt.ylim(0, 0.004)
                 plt.ylabel("Percentage")
 
                 outputfolder = "D:\\data\\Aaron\\ProstateMRL\\Data\\Extraction\\Histograms\\Raw\\20fractions_new\\" + str(i)
