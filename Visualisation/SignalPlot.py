a88d73fbcbef47116b34070c01bbc58d673526ba
aaron-rankin <aaron.rankin@student.manchester.ac.uk>
2023-02-28 16:06:49 +0000

    Abstract

--- Visualisation/SignalPlot.py
+++ Visualisation/SignalPlot.py
@@ -7,10 +7,9 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent + "\\Functions\\")
 import UsefulFunctions as UF
-import PlottingFunctions as PF
 root = UF.DataRoot(2)
 
-df_fts = pd.read_csv(root + "Aaron\ProstateMRL\Data\Paper1\FeaturesHM\All_fts_change.csv")
+df_fts = pd.read_csv(root + "Aaron\ProstateMRL\Data\Paper1\Raw\\Features\\Longitudinal_All_fts.csv")
 print(df_fts.head())
 
 
@@ -21,6 +20,12 @@ PatIDs = df_fts["PatID"].unique()
 # plot the median signal intensity for each patient using seaborn facetgrid
 g = sns.FacetGrid(df_fts, col="PatID", col_wrap=5, height=2, aspect=1.5)
 g.map(plt.plot, "Fraction", "FeatureValue", marker="o")
-g.set_axis_labels("Fraction", "Median Signal Intensity")
+g.set_axis_labels("Fraction", "Signal", fontsize=16)
+# change fontsize of x and y labels
+# change fontsize of x and y ticks
+plt.tick_params(labelsize=16)
+# set title of total figure
+g.fig.suptitle("Median Prostate Signal Intensity", fontsize=30, y=1.0)
+g.figure.subplots_adjust(top=0.9)
 g.set_titles("{col_name}")
 plt.show()
